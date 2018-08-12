b0b3102dc83f8b95f08218963efda90803ef9f28
uyoungco <uyoung.co@gmail.com>
2018-08-12 23:26:32 +0800

    first commit

---
+++ src/common/header/index.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { CSSTransition } from 'react-transition-group'
+import {
+  HeaderWrapper,
+  Logo,
+  Nav,
+  NavItem,
+  NavSearch,
+  Addition,
+  Button,
+  SearchWrapper
+} from './style'
+
+
+const Header = (props) => {
+  return (
+    <HeaderWrapper>
+      <Logo />
+      <Nav>
+        <NavItem className="left active">首页</NavItem>
+        <NavItem className="left">下载APP</NavItem>
+        <NavItem className="right">登录</NavItem>
+        <NavItem className="right">
+          <i className="iconfont">&#xe636;</i>
+        </NavItem>
+        <SearchWrapper>
+          <CSSTransition
+            in={props.focused}
+            timeout={200}
+            classNames="slide"
+          >
+            <NavSearch
+              className={props.focused ? 'focused' : ''}
+              onFocus={() => props.handleInputFocus()}
+              onBlur={() => props.handleInputBlur()}
+            ></NavSearch>
+          </CSSTransition>
+          <i
+            className={props.focused ? 'focused iconfont' : 'iconfont'}
+          >&#xe60d;</i>
+        </SearchWrapper>
+      </Nav>
+      <Addition>
+        <Button className='reg'>注册</Button>
+        <Button className='writting'>
+          <i className="iconfont">&#xe615;</i>
+          写文章
+        </Button>
+      </Addition>
+    </HeaderWrapper>
+  )
+}
+
+
+const mapStateToProps = (state) => {
+  return {
+    focused: state.focused
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleInputFocus() {
+      const action = {
+        type: 'search_focus'
+      }
+      dispatch(action)
+    },
+    handleInputBlur() {
+      const action = {
+        type: 'search_blur'
+      }
+      dispatch(action)
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
