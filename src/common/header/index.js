caea04be39b5f98d7642f714bc1789d5ab3b7b85
uyoungco <uyoung.co@gmail.com>
2018-08-13 00:46:30 +0800

    使用Immutable来管理store中的数据

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -57,7 +57,7 @@ const Header = (props) => {
 
 const mapStateToProps = (state) => {
   return {
-    focused: state.header.focused
+    focused: state.header.get('focused')
   }
 }
 const mapDispatchToProps = (dispatch) => {
